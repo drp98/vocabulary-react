510ce2b1fcd5f820dcbd6288943fd36ec1a3955b
Roman Dziadyk <dziadyk.roman@gmail.com>
2020-12-24 06:16:33 +0200

    replace buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react'
 import NavBar from './NavBar'
-import Word from './Word'
-
-import { Button, ButtonGroup } from 'react-bootstrap'
+import Words from './Words'
 
 const App = () => {
     return (
         <>
             <NavBar />
-            <Word />
-
-            <div className='text-center col p-0'>
-                <Button variant='outline-primary' className='w-50 mb-2'>Translate</Button><br />
-                <ButtonGroup className='text-center w-50'>
-                    <Button variant='outline-primary'>Back</Button>
-                    <Button variant='outline-primary'>Next</Button>
-                </ButtonGroup>
-            </div>
+            <Words />
         </>
     )
 }
