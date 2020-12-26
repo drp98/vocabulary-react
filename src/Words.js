7f7837dd3264e28c31ac1cfbe3fe5f6cffd584d8
Roman Dziadyk <dziadyk.roman@gmail.com>
2020-12-26 14:09:52 +0200

    heroku test

--- src/Words.js
+++ src/Words.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Card, Button, ButtonGroup } from 'react-bootstrap'
+import './Words.css'
 
 const initialWords = [
     {
@@ -25,10 +26,11 @@ const initialWords = [
 ]
 
 const Words = () => {
-    const [ words, setWords ] = useState({})
-    const [ currentWord, setCurrentWord ] = useState({})
+    const [words, setWords] = useState({})
+    const [currentWord, setCurrentWord] = useState({})
     const [index, setIndex] = useState(0)
     const [isTranslated, setIsTranslated] = useState(false)
+    const wordsRef = useRef({})
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
@@ -36,54 +38,53 @@ const Words = () => {
             .then(json => {
                 setWords(initialWords)
                 setCurrentWord(initialWords[0])
+                wordsRef.current = initialWords
             })
     }, [])
 
     useEffect(() => {
-        setCurrentWord(words[index])
+        setCurrentWord(wordsRef.current[index])
         setIsTranslated(false)
-    }, [index, words])
+    }, [index])
 
     const next = () => {
-        ( index < words.length - 1 )
-        ? setIndex(index + 1)
-        : setIndex(0)
+        index < words.length - 1 ? setIndex(index + 1) : setIndex(0)
     }
 
     const back = () => {
-        ( index > 0 )
-        ? setIndex(index - 1)
-        : setIndex(words.length - 1)
+        index > 0 ? setIndex(index - 1) : setIndex(words.length - 1)
     }
 
     const translate = () => setIsTranslated(!isTranslated)
 
     return (
-        <div className='container text-center mt-3 mb-3 p-0 w-50'>
+        <div
+            id='width'
+            className='container-sm text-center mt-3 mb-3 p-0 pr-3 pl-3'
+        >
             <Card>
-                <br />
-                <span>
+                <p className='mt-3'>
                     [ {index + 1} / {words?.length} ]
-                </span>
-                <br />
+                </p>
 
-                <span className='font-weight-bold display-4'>
+                <p className='font-weight-bold display-4'>
                     {currentWord?.word}
-                </span>
-                <br />
+                </p>
+
+                <p className='h3 font-weight-light mt-4'>
+                    {currentWord?.transcription}
+                </p>
+
                 <p
-                    className={`h4 font-weight-light ${
+                    id='test'
+                    className={`h4 font-weight-light mt-4 ${
                         isTranslated || 'invisible'
                     }`}
                 >
                     {currentWord?.translate}
                 </p>
-                <br />
-                <span className='h3 font-weight-light'>
-                    {currentWord?.transcription}
-                </span>
-                <br />
-                <Button variant='outline-warning font-weight-bold'>
+
+                <Button variant='outline-warning font-weight-bold mt-4'>
                     Already know
                 </Button>
             </Card>
