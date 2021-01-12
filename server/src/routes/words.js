d91ec7f5c06fcbf428c947211a1a128cb8c2728d
Roman Dziadyk <dziadyk.roman@gmail.com>
2021-01-13 01:42:42 +0200

    added api/words/length route

--- server/src/routes/words.js
+++ server/src/routes/words.js
@@ -3,6 +3,18 @@ import Word from '../models/word'
 
 const router = express.Router()
 
+router.get('/length', async (req, res) => {
+    await Word.countDocuments()
+        .then(length => {
+            res.json(length)
+            console.log(length)
+        })
+        .catch(err => {
+            res.status(500).json({ errors: { global: err } })
+            console.log('Caught. Error :', err.message)
+        })
+})
+
 router.get('/:_id', async (req, res) => {
     await Word.find({ id: req.params._id })
         .then(word => {
@@ -10,7 +22,7 @@ router.get('/:_id', async (req, res) => {
         })
         .catch(err => {
             res.status(500).json({ errors: { global: err } })
-            console.log('Caught. Error in :', err.message)
+            console.log('Caught. Error :', err.message)
         })
 })
 
